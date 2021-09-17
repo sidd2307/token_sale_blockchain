507842eaf150111934fff98977e2c0d2f03a1830
Siddheshwar Panda <siddheshwar.2307@gmail.com>
2021-09-17 18:14:22 +0530

    1. Smoke Test

---
+++ contracts/DappToken.sol
@@ -0,0 +1,14 @@
+pragma solidity >=0.4.22 <0.9.0;
+
+contract DappToken {
+    // No of token available
+
+    // Constructor
+    uint256 public totalSupply;
+
+    constructor() public {
+        totalSupply = 1000000;
+    }
+    // Set the total number of tokens
+    // Read the total number of tokens
+}
---
+++ truffle.js
@@ -0,0 +1,10 @@
+module.exports = {
+    // networks
+    networks: {
+        development: {
+            host: "127.0.0.1",
+            port: "7545",
+            network_id: "*" // match any network id
+        }
+    }
+}
